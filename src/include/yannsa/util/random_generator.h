7f2086f40dded35318630bcfd65b8a616827e748
shallyan <yanhust@qq.com>
2017-06-16 10:43:00 +0800

    Remove useless gauss random generator

--- src/include/yannsa/util/random_generator.h
+++ src/include/yannsa/util/random_generator.h
@@ -23,22 +23,6 @@ class IntRandomGenerator {
     std::mt19937 random_generator_;
 };
 
-template <typename CoordinateType>
-class GaussRealRandomGenerator {
-  public:
-    GaussRealRandomGenerator(CoordinateType mean, CoordinateType stddev) 
-        : distribution_generator_(mean, stddev), random_generator_(/*std::random_device()()*/2) {
-    }
-
-    CoordinateType Random() {
-      return distribution_generator_(random_generator_);
-    }
-  
-  private:
-    std::normal_distribution<CoordinateType> distribution_generator_;
-    std::mt19937 random_generator_;
-};
-
 } // namespace util
 } // namespace yannsa
 
